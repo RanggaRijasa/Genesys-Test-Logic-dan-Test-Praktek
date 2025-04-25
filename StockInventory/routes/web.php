9a93e12aa54037feb92963ff2a4e396a58369663
RanggaRijasa <127366481+RanggaRijasa@users.noreply.github.com>
2025-04-25 15:49:32 +0700

    Update web.php

--- StockInventory/routes/web.php
+++ StockInventory/routes/web.php
@@ -10,25 +10,20 @@ use App\Http\Controllers\SaleController;
 Route::post('/sale', [SaleController::class, 'store']);
 Route::post('/purchases', [PurchaseController::class, 'store']);
 
-// Home diarahkan ke login (halaman default Breeze)
 Route::get('/', function () {
-    return view('auth.login'); // Gunakan 'auth.login' untuk Breeze
+    return view('auth.login'); 
 });
 
-// Dashboard (setelah login)
 Route::get('/dashboard', [InventoryController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-
-// Fitur yang hanya bisa diakses setelah login
 Route::middleware('auth')->group(function () {
-    // Profil user
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Inventory dan Transaksi
     Route::resource('inventory', InventoryController::class);
     Route::post('/purchase', [PurchaseController::class, 'store']);
     Route::post('/sale', [SaleController::class, 'store']);
